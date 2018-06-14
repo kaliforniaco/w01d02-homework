8159149bc83139fe9bfd0583d3f16f3326e6c7a0
David Epps <kaliforniaco@gmail.com>
2018-06-14 17:09:32 -0600

    Commit 9 - Created loops.js file

---
+++ resume_page/js/loops.js
@@ -0,0 +1,43 @@
+console.log("Loops is online");
+
+for (i=0;i<100;i++){
+	console.log(i);
+}
+
+//- Ascending 1-99
+
+for (i=99;i>-1;i--){
+	console.log(i);
+}
+
+//- Descending 99-1
+
+for (i=0;i<100;i+=2){
+	console.log(i);
+}
+
+//- Ascending Even
+
+for (i=98;i>1;i-=2){
+	console.log(i);
+}
+
+//- Descending Even
+
+for (i=49;i<73;i++){
+	console.log(i);
+}
+
+//- Ascending 49-72
+
+for (i=81;i>25;i--){
+	console.log(i);
+}
+
+//- Decsending 81-26
+
+for (i=3;i<91;i+=3){
+	console.log(i);
+}
+
+//- Ascending in Mult*3
